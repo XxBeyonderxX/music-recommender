cb6d2f12af1b668737940e7fbc074c4a941c2969
Mudit Tyagi <mudittyagi@Mudits-MacBook-Air.local>
2023-06-02 15:56:22 +0530

    modified based_on_text

--- model/based_on_text.py
+++ model/based_on_text.py
@@ -1,27 +1,61 @@
+import os
+import sys
+import json
+import numpy as np
+import pandas as pd
 import tensorflow as tf
 import tensorflow_hub as hub
-import pandas as pd
-import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.neighbors import NearestNeighbors
-df=pd.read_csv("spotify_millsongdata.csv")
-df.reset_index()
-model=hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
-def create_embed(df):
-        return model(df)
-lyrics=list(df['text'])
-lyrics=lyrics[:5000]
-emb=create_embed(lyrics)
-nn=NearestNeighbors(n_neighbors=5)
-nn.fit(emb)
-def recommend(text):
-    textemb=create_embed([text])
-    neighbors=nn.kneighbors(textemb,return_distance=False)[0]
-    art=df['artist'].iloc[neighbors].tolist()
-    song=df['song'].iloc[neighbors].tolist()
-    art_song={}
-    for i in range(len(art)):
-        art_song[song[i]]=art[i]
-    return art_song
-text=input("Search ")
-recommend(text)
+
+
+# sys.stdout = open('/dev/null', 'w')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+def preprocessing():
+
+    
+    # with tf.device('/device:GPU:0'):
+        df = pd.read_csv('/Users/mudittyagi/Coding/PRACTICUM/data/lyrics.csv')
+        df.reset_index()
+        model = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+        # model = hub.load("https://tfhub.dev/google/universal-sentence-encoder-lite/2")
+
+        def create_embed(texts):
+            batch_size = 1024 
+            # Adjust the batch size as needed
+            embeddings = []
+            for i in range(0, len(texts), batch_size):
+                batch = texts[i:i+batch_size]
+                embeddings.append(model(batch))
+            return tf.concat(embeddings, axis=0)
+
+        lyrics = list(df['text'])
+        # lyrics = lyrics[:10000]  
+        # Adjust the number of lyrics as needed
+        emb = create_embed(lyrics)
+
+        nn = NearestNeighbors(n_neighbors=5)
+        nn.fit(emb)
+
+        def recommend(text):
+            textemb=create_embed([text])
+            neighbors=nn.kneighbors(textemb,return_distance=False)[0]
+            artist=[]
+            tracks=[]
+            
+            artist.append(df['artist'].iloc[neighbors].to_list())
+            tracks.append(df['song'].iloc[neighbors].to_list())
+
+            artist = artist[0]
+            tracks = tracks[0]
+            # print(artist) 
+            # print(tracks)
+
+            result = {'artist':artist, 'tracks':tracks}
+            print(json.dumps(result))
+
+        recommend(sys.argv[1])
+
+preprocessing()
+# print(sys.argv[1])
